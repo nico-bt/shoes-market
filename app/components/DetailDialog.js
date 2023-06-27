5652b5ad07ea3cd2253bea86dd8e7fc722023d61
Nico-Battaglia <nico-battaglia@hotmail.com>
2023-06-27 03:20:06 -0300

    Display imgs in a carousel and dinamically display buy buttons only if money is enought

--- app/components/DetailDialog.js
+++ app/components/DetailDialog.js
@@ -1,17 +1,16 @@
 "use client"
 
-import * as React from "react"
 import Button from "@mui/material/Button"
 import Dialog from "@mui/material/Dialog"
 import DialogActions from "@mui/material/DialogActions"
-import CloseIcon from "@mui/icons-material/Close"
+import closeIcon from "../../public/closeIcon.svg"
 import Image from "next/image"
-import { CircularProgress, DialogTitle, IconButton } from "@mui/material"
+import { DialogTitle, IconButton } from "@mui/material"
 import { useUserContext } from "../context/userContext"
 import ImgCarousel from "./ImgCarousel"
 
-export default function DetailDialog({ open, setOpen, item }) {
-  const { getUser, redeemItem, isTransactionOk } = useUserContext()
+export default function DetailDialog({ open, setOpen, item, canBuy }) {
+  const { redeemItem } = useUserContext()
 
   const handleClose = () => {
     setOpen(false)
@@ -30,12 +29,13 @@ export default function DetailDialog({ open, setOpen, item }) {
           onClick={handleClose}
           sx={{
             position: "absolute",
-            right: 8,
-            top: 8,
-            color: (theme) => theme.palette.grey[500],
+            right: 1,
+            top: 1,
+            zIndex: 900,
+            fill: "gray",
           }}
         >
-          <Image src={CloseIcon} width={32} height={32} alt="close icon" />
+          <Image src={closeIcon} width={24} height={24} alt="close icon" />
         </IconButton>
       </DialogTitle>
     )
@@ -50,34 +50,34 @@ export default function DetailDialog({ open, setOpen, item }) {
       fullWidth
       maxWidth="xs"
     >
-      <DialogActions
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-        }}
-      >
-        {/* <img src={item.images[0]} width={"100%"} alt={`${item.title} image`} /> */}
-
+      <CloseBtn />
+      <DialogActions sx={{ display: "flex", flexDirection: "column" }}>
         <ImgCarousel images={item.images} />
 
         <h2 style={{ margin: 0, marginBottom: "8px", textAlign: "center" }}>{item.title}</h2>
-        <div
-          style={{
-            display: "flex",
-            fontSize: "2rem",
-          }}
-        >
-          {item.price}
-        </div>
 
-        <div style={{ display: "flex", gap: 12, marginTop: 6 }}>
-          <Button variant="outlined" size="large" onClick={handleClose}>
-            Cancel
-          </Button>
-          <Button variant="contained" size="large" autoFocus onClick={handleGetItem}>
-            Comprar
-          </Button>
-        </div>
+        <div style={{ display: "flex", fontSize: "2rem" }}>$ {item.price.toLocaleString()}</div>
+
+        {canBuy && (
+          <div style={{ display: "flex", gap: 12, marginTop: 6 }}>
+            <Button variant="outlined" color="error" size="large" onClick={handleClose}>
+              Cancelar
+            </Button>
+            <Button
+              variant="contained"
+              color="error"
+              size="large"
+              autoFocus
+              onClick={handleGetItem}
+              style={{
+                backgroundImage:
+                  "linear-gradient(rgba(255, 225, 0, 0.75),rgba(255, 165, 0, 0.75), rgba(255, 0, 0, 0.75))",
+              }}
+            >
+              Comprar
+            </Button>
+          </div>
+        )}
       </DialogActions>
     </Dialog>
   )
