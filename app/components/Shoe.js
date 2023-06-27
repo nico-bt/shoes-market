9f9af172224a6d0d44d8f4f3567f3c3f9ab762c9
Nico-Battaglia <nico-battaglia@hotmail.com>
2023-06-27 00:05:58 -0300

    Display dinamically buy overlay, depending on user's money

--- app/components/Shoe.js
+++ app/components/Shoe.js
@@ -1,8 +1,24 @@
 import Image from "next/image"
 import styles from "./shoe.module.css"
+import { useUserContext } from "../context/userContext"
+import { useEffect, useState } from "react"
 
 function Shoe({ item }) {
-  const { title, brand, price, thumbnail, images } = item
+  const { id, title, brand, price, thumbnail, images } = item
+
+  const { user, redeemItem } = useUserContext()
+  const [canBuy, setCanBuy] = useState(true)
+
+  useEffect(() => {
+    setCanBuy(user.money - price > 0)
+    console.log(user)
+  }, [user])
+
+  const handleClick = () => {
+    if (canBuy) {
+      redeemItem(item)
+    }
+  }
 
   return (
     <div className={styles.productItem}>
@@ -14,10 +30,20 @@ function Shoe({ item }) {
         <h2>{title}</h2>
       </div>
 
-      <div className={styles.productItemOverlay}>
-        <h2>$ {price}</h2>
-        <button>Comprar</button>
-      </div>
+      {canBuy && (
+        <div className={styles.productItemOverlay}>
+          <h2>$ {price}</h2>
+          <button onClick={handleClick} disabled={!canBuy}>
+            Comprar
+          </button>
+        </div>
+      )}
+
+      {!canBuy && (
+        <div className={styles.moreMoney}>
+          Te faltan ${Math.abs(user.money - price).toLocaleString()}
+        </div>
+      )}
     </div>
   )
 }
