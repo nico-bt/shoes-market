62b5b7ccd6534ddb1ff627616172c8903356ce18
Nico-Battaglia <nico-battaglia@hotmail.com>
2023-06-27 15:20:22 -0300

    Add quantity per item and proper logic. Add removeItem, and now starts with 5000

--- app/context/userContext.js
+++ app/context/userContext.js
@@ -41,17 +41,51 @@ const reducer = (state, action) => {
         // Sin suficiente dinero en cuenta
         break
       }
+
+      // Ya está en el cart el Producto?
+      const productInCartIndex = state.user.cart.findIndex(
+        (cartItem) => cartItem.id === action.payload.product.id
+      )
+      let newCart
+      // Si el producto ya está en el cart, le sumamos +1
+      if (productInCartIndex >= 0) {
+        newCart = state.user.cart.map((cartItem) => {
+          if (cartItem.id === action.payload.product.id) {
+            return { ...cartItem, quantity: cartItem.quantity + 1 }
+          } else {
+            return cartItem
+          }
+        })
+      } else {
+        // Si no está en el cart lo agregamos con quant:1
+        newCart = [...state.user.cart, { ...action.payload.product, quantity: 1 }]
+      }
+
       const newState = {
         ...state,
         isLoading: false,
         user: {
           ...state.user,
           money: state.user.money - action.payload.product.price,
-          cart: [...state.user.cart, action.payload.product],
+          cart: newCart,
         },
       }
+
       localStorage.setItem("user", JSON.stringify(newState.user))
       return newState
+
+    case "REMOVE_ITEM":
+      const newCartState = {
+        ...state,
+        isLoading: false,
+        user: {
+          ...state.user,
+          money: state.user.money + action.payload.product.price * action.payload.product.quantity,
+          cart: state.user.cart.filter((prod) => prod.id !== action.payload.product.id),
+        },
+      }
+      localStorage.setItem("user", JSON.stringify(newCartState.user))
+      return newCartState
 
     default:
       break
@@ -62,7 +96,7 @@ const reducer = (state, action) => {
 // ***********************************************************
 // LOCAL VERSION:
 const INITIAL_USER = {
-  money: 6000,
+  money: 5000,
   cart: [],
 }
 
@@ -112,6 +146,12 @@ const UserProvider = ({ children }) => {
     dispatch({ type: "REDEEM_ITEM_CANCEL" })
   }
 
+  // Remove from Cart
+  //--------------------------------------------
+  const removeItem = (product) => {
+    dispatch({ type: "REMOVE_ITEM", payload: { product } })
+  }
+
   // Set First Load
   //-------------------------------------------------------------------------
   const setFirstLoadFalse = () => {
@@ -124,6 +164,7 @@ const UserProvider = ({ children }) => {
         ...state,
         redeemItemCancel,
         redeemItem,
+        removeItem,
         setFirstLoadFalse,
       }}
     >
